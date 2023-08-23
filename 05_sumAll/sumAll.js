c33b1093eaad686dd31ddcb0d3e9c72b5d6fdd6a
maxkonrad <furkanselek@outlook.com>
2023-08-24 00:42:33 +0300

    implement sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a >= 0 && b >= 0 && typeof a == "number" && typeof b == "number"){
+        return b > a ? (b * (b + 1) / 2) - a + 1 : (a * (a + 1) / 2) - b + 1
+    } else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
