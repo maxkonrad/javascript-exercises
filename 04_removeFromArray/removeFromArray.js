9aa41aabb766a897975c966fb4be792b9e82a3db
maxkonrad <furkanselek@outlook.com>
2023-08-24 00:29:11 +0300

    implement removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...el) {
+    const newArr = []
+    arr.forEach(element => {
+        if (!el.includes(element)) {
+            newArr.push(element);
+        }
+    });
 
+    return newArr
 };
 
 // Do not edit below this line
