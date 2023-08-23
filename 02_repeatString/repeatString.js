df27f2065103d064115b7032083b292542924d03
maxkonrad <furkanselek@outlook.com>
2023-08-24 00:03:17 +0300

    add string repeating function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num >= 0) return str.repeat(num)
+    else return 'ERROR'
 };
 
 // Do not edit below this line
